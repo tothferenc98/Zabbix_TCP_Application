b00804e8fb630d449e8e4d2539d5d3b04da067c2
tothferenc98 <tothferenc626@gmail.com>
2021-06-24 13:08:33 +0200

    Add project files.

---
+++ teszt21/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teszt21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region konstansok
+            const string HOSTNAME = "gyakornok_tf_app";
+            const string ZABBIX_NAME = "zabbix.beks.hu";
+            const int ZABBIX_PORT  = 10051;
+            #endregion konstansok
+
+            // TODO: TCP kapcsolat meghatározása  (TASK LIST)
+            // TODO: TcpClient példakód átmásolása https://docs.microsoft.com/en-us/dotnet/api/system.net.sockets.tcpclient?view=net-5.0
+
+            // ZABBIX request string megépítése (1624524243) epoch időkonverzió - string.Format!!
+            // Végtelen ciklus 20s-ként megszólítani a zabbix szervert (hibakezelés)
+            // válasz kiíratása konzolra
+            // Rövid függvények
+
+
+        }
+    }
+}
