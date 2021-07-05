8f8022b50f74c9c719f724c2d4804569f4c729a6
tothferenc98 <tothferenc626@gmail.com>
2021-07-05 16:58:42 +0200

    Az active check által kért adatok küldése a Zabbix számára, hibakezelés, függvények létrehozása adatok lekéréséhez és a main szétszedése függvényekre.

--- teszt21/Program.cs
+++ teszt21/Program.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Management;
 using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
-using System.Text.Json;
 using Newtonsoft.Json;
+
 
 
 #region feladatok
@@ -33,91 +35,123 @@ namespace Zabbix_TCP_Application
         const string HOSTNAME = "gyakornok_tf_app";
         const string ZABBIX_NAME = "zabbix.beks.hu";
         const int ZABBIX_PORT = 10051;
-        const int CONNECT_DELAY = 5;
+        const int CONNECT_DELAY = 20;
         #endregion konstansok
-        
 
         static void Main(string[] args)
         {
             
             while (true)
             {
-                var clock = DateTimeOffset.Now.ToUnixTimeSeconds();
-                var rand = new Random();
-                int ns = rand.Next(000000001, 999999999);
-                #region stringek
-                string agentHostname = String.Format("{{\"host\":\"{0}\",\"key\":\"agent.hostname\",\"value\":\"{0}\",\"id\":1,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
-                string agentPing = String.Format("{{\"host\":\"{0}\",\"key\":\"agent.ping\",\"value\":\"1\",\"id\":2,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
-                string agentVersion = String.Format("{{\"host\":\"{0}\",\"key\":\"agent.version\",\"value\":\"TCP_program\",\"id\":3,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
-                
-                string eventlogSystemMicrosoftWindowsKernelPower = String.Format("{{\"host\":\"{0}\",\"key\":\"eventlog[System,,,\\\"Microsoft-Windows-Kernel-Power\\\"]\",\"value\":\"A felhaszn..l..i ..zemm..d.. folyamat megpr..b..lta m..dos..tani a rendszer..llapotot a SetSuspendState vagy a SetSystemPowerState API h..v..s..val.\",\"lastlogsize\":10815,\"timestamp\":1624900534,\"source\":\"Microsoft-Windows-Kernel-Power\",\"severity\":1,\"eventid\":187,\"id\":4,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
-                string netIfList = String.Format("{{\"host\":\"{0}\",\"key\":\"net.if.list\",\"value\":\"Teszt\",\"id\":8,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
-                string perfCounter234Total1402 = String.Format("{{\"host\":\"{0}\",\"key\":\"perf_counter[\\\\234(_Total)\\\\1402]\",\"value\":\"0.019797\",\"id\":9,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns); //Average disk read queue length
-                string perfCounter234Total1404 = String.Format("{{\"host\":\"{0}\",\"key\":\"perf_counter[\\\\234(_Total)\\\\1404]\",\"value\":\"0.006441\",\"id\":10,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns); //Average disk write queue length
-                string perfCounter2_16 = String.Format("{{\"host\":\"{0}\",\"key\":\"perf_counter[\\\\2\\\\16]\",\"value\":\"17219.955967\",\"id\":11,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns); //File read bytes per second
-                string perfCounter2_18 = String.Format("{{\"host\":\"{0}\",\"key\":\"perf_counter[\\\\2\\\\18]\",\"value\":\"113342.473095\",\"id\":12,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns); //File write bytes per second
-                string perfCounter2_250 = String.Format("{{\"host\":\"{0}\",\"key\":\"perf_counter[\\\\2\\\\250]\",\"value\":\"2906.000000\",\"id\":13,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
-                string procNum = String.Format("{{\"host\":\"{0}\",\"key\":\"proc.num[]\",\"value\":\"{3}\",\"id\":14,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns, GetProcessNumber());
-                string systemCpuLoadPerCpuAvg1 = String.Format("{{\"host\":\"{0}\",\"key\":\"system.cpu.load[percpu,avg1]\",\"value\":\"0.000000\",\"id\":15,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
-                string systemCpuLoadPerCpuAvg5 = String.Format("{{\"host\":\"{0}\",\"key\":\"system.cpu.load[percpu,avg5]\",\"value\":\"0.000000\",\"id\":16,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
-                string systemLocaltimeUtc = String.Format("{{\"host\":\"{0}\",\"key\":\"system.localtime[utc]\",\"value\":\"{1}\",\"id\":17,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
-                string systemRunIpconfigFindstrIPv4sort = String.Format("{{\"host\":\"{0}\",\"key\":\"system.run[ipconfig | findstr IPv4 | sort]\",\"value\":\"Unsupported item key.\",\"state\":1,\"id\":18,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
-                string systemRunSysteminfo = String.Format("{{\"host\":\"{0}\",\"key\":\"system.run[systeminfo,]\",\"value\":\"Unsupported item key.\",\"state\":1,\"id\":19,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
-                string systemSwapSizeFree = String.Format("{{\"host\":\"{0}\",\"key\":\"system.swap.size[,free]\",\"value\":\"1971662848\",\"id\":20,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
-                string systemSwapSizeTotal = String.Format("{{\"host\":\"{0}\",\"key\":\"system.swap.size[,total]\",\"value\":\"5637144576\",\"id\":21,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
-                string systemUname = String.Format("{{\"host\":\"{0}\",\"key\":\"system.uname\",\"value\":\"{3}\",\"id\":22,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns, GetSystemUname());
-                string systemUptime = String.Format("{{\"host\":\"{0}\",\"key\":\"system.uptime\",\"value\":\"609411\",\"id\":23,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
-                string vmMemorySizeFree = String.Format("{{\"host\":\"{0}\",\"key\":\"vm.memory.size[free]\",\"value\":\"{3}\",\"id\":24,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns, GetAvailableMemoryInBytes());
-                string vmMemorySizeTotal = String.Format("{{\"host\":\"{0}\",\"key\":\"vm.memory.size[total]\",\"value\":\"{3}\",\"id\":25,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns, GetTotalMemoryInBytes());
+                string message = String.Format("{{\"request\":\"active checks\",\"host\":\"{0}\"}}", HOSTNAME);
+                string responseData = MakePacketAndConnect(message);
 
-                #endregion stringek
-                
-                Console.WriteLine();
-                Console.ReadLine();
-
-                string message = "{\"request\":\"agent data\",\"session\":\"2dcf1bf2f6fc1c742812fbbf491e24f2\",\"data\":[" 
-                    + agentHostname + "," + agentPing + "," + agentVersion + "," + eventlogSystemMicrosoftWindowsKernelPower + "," + netIfList + "," + perfCounter234Total1402 + "," + perfCounter234Total1404 +
-                    "," + perfCounter2_16 + "," + perfCounter2_18 + "," + perfCounter2_250 + "," + procNum + "," + systemCpuLoadPerCpuAvg1 + "," + systemCpuLoadPerCpuAvg5 + "," + systemLocaltimeUtc +
-                     "," + systemRunIpconfigFindstrIPv4sort + "," + systemRunSysteminfo + "," + systemSwapSizeFree + "," + systemSwapSizeTotal + "," + systemUname + "," + systemUptime + "," + vmMemorySizeFree + "," + vmMemorySizeTotal +
-                    "],\"clock\":" + clock + ",\"ns\":" + ns + "}";
-                //string message = "{\"request\":\"active checks\",\"host\":\"gyakornok_tf_app\"}";
-
-                byte[] packet = Packet(message);
-
-                string responseData = "";
-                try
+                if (responseData.Contains("success"))
                 {
-                    responseData = Connect(ZABBIX_NAME, packet, message);
-                    
+                    MakeAgentDataMessage(responseData);
                 }
-                catch (Exception e)
-                {
-                    Console.WriteLine("HIBA: {0}",e);
-                }
-                Console.WriteLine("Received: {0}\n", responseData);
-                
 
-                // active check-re működik
-                /*List<JsonClass> jsonData = JsonConvert.DeserializeObject<List<JsonClass>>(ActiveCheckFilter(responseData));
-                foreach (var item in jsonData)
-                {
-                    Console.WriteLine(item.key);
-                }*/
+                System.Threading.Thread.Sleep(CONNECT_DELAY * 1000);
 
-                System.Threading.Thread.Sleep(CONNECT_DELAY*1000);
-                
-
-                //Console.WriteLine("Várakozás, folytatáshoz nyomj entert");
-                Console.ReadKey();
             }
-
         }
 
-       
-        
+        public static string MakeAgentDataMessage(string responseData) {
+            #region változók
+            var clock = DateTimeOffset.Now.ToUnixTimeSeconds();
+            var rand = new Random();
+            int ns = rand.Next(000000001, 999999999);
+
+            string agentHostname = String.Format("{{\"host\":\"{0}\",\"key\":\"agent.hostname\",\"value\":\"{0}\",\"id\":1,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
+            string agentPing = String.Format("{{\"host\":\"{0}\",\"key\":\"agent.ping\",\"value\":\"1\",\"id\":2,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
+            string agentVersion = String.Format("{{\"host\":\"{0}\",\"key\":\"agent.version\",\"value\":\"TCP_program\",\"id\":3,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
+            string eventlogSystemMicrosoftWindowsKernelPower = String.Format("{{\"host\":\"{0}\",\"key\":\"eventlog[System,,,\\\"Microsoft-Windows-Kernel-Power\\\"]\",\"value\":\"A felhaszn..l..i ..zemm..d.. folyamat megpr..b..lta m..dos..tani a rendszer..llapotot a SetSuspendState vagy a SetSystemPowerState API h..v..s..val.\",\"lastlogsize\":10815,\"timestamp\":1624900534,\"source\":\"Microsoft-Windows-Kernel-Power\",\"severity\":1,\"eventid\":187,\"id\":4,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
+            string netIfList = String.Format("{{\"host\":\"{0}\",\"key\":\"net.if.list\",\"value\":\"Teszt\",\"id\":8,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
+            string perfCounter234Total1402 = String.Format("{{\"host\":\"{0}\",\"key\":\"perf_counter[\\\\234(_Total)\\\\1402]\",\"value\":\"0.019797\",\"id\":9,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns); //Average disk read queue length
+            string perfCounter234Total1404 = String.Format("{{\"host\":\"{0}\",\"key\":\"perf_counter[\\\\234(_Total)\\\\1404]\",\"value\":\"0.006441\",\"id\":10,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns); //Average disk write queue length
+            string perfCounter2_16 = String.Format("{{\"host\":\"{0}\",\"key\":\"perf_counter[\\\\2\\\\16]\",\"value\":\"17219.955967\",\"id\":11,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns); //File read bytes per second
+            string perfCounter2_18 = String.Format("{{\"host\":\"{0}\",\"key\":\"perf_counter[\\\\2\\\\18]\",\"value\":\"113342.473095\",\"id\":12,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns); //File write bytes per second
+            string perfCounter2_250 = String.Format("{{\"host\":\"{0}\",\"key\":\"perf_counter[\\\\2\\\\250]\",\"value\":\"{3}\",\"id\":13,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns, GetPerformanceCounter2_250()); //Number of threads
+            string procNum = String.Format("{{\"host\":\"{0}\",\"key\":\"proc.num[]\",\"value\":\"{3}\",\"id\":14,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns, GetProcessNumber());
+            string systemCpuLoadPerCpuAvg1 = String.Format("{{\"host\":\"{0}\",\"key\":\"system.cpu.load[percpu,avg1]\",\"value\":\"0.000000\",\"id\":15,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
+            string systemCpuLoadPerCpuAvg5 = String.Format("{{\"host\":\"{0}\",\"key\":\"system.cpu.load[percpu,avg5]\",\"value\":\"0.000000\",\"id\":16,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
+            string systemLocaltimeUtc = String.Format("{{\"host\":\"{0}\",\"key\":\"system.localtime[utc]\",\"value\":\"{1}\",\"id\":17,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
+            string systemRunIpconfigFindstrIPv4sort = String.Format("{{\"host\":\"{0}\",\"key\":\"system.run[ipconfig | findstr IPv4 | sort]\",\"value\":\"Unsupported item key.\",\"state\":1,\"id\":18,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
+            string systemRunSysteminfo = String.Format("{{\"host\":\"{0}\",\"key\":\"system.run[systeminfo,]\",\"value\":\"Unsupported item key.\",\"state\":1,\"id\":19,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
+            string systemSwapSizeFree = String.Format("{{\"host\":\"{0}\",\"key\":\"system.swap.size[,free]\",\"value\":\"1971662848\",\"id\":20,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
+            string systemSwapSizeTotal = String.Format("{{\"host\":\"{0}\",\"key\":\"system.swap.size[,total]\",\"value\":\"5637144576\",\"id\":21,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns);
+            string systemUname = String.Format("{{\"host\":\"{0}\",\"key\":\"system.uname\",\"value\":\"{3}\",\"id\":22,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns, GetSystemUname());
+            string systemUptime = String.Format("{{\"host\":\"{0}\",\"key\":\"system.uptime\",\"value\":\"{3}\",\"id\":23,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns, GetUpTime());
+            string vmMemorySizeFree = String.Format("{{\"host\":\"{0}\",\"key\":\"vm.memory.size[free]\",\"value\":\"{3}\",\"id\":24,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns, GetAvailableMemoryInBytes());
+            string vmMemorySizeTotal = String.Format("{{\"host\":\"{0}\",\"key\":\"vm.memory.size[total]\",\"value\":\"{3}\",\"id\":25,\"clock\":{1},\"ns\":{2}}}", HOSTNAME, clock, ns, GetTotalMemoryInBytes());
+
+            Dictionary<string, string> stringpair = new Dictionary<string, string>();
+            stringpair.Add("agent.hostname", agentHostname);
+            stringpair.Add("agent.ping", agentPing);
+            stringpair.Add("agent.version", agentVersion);
+            stringpair.Add("eventlog[System,,,\"Microsoft-Windows-Kernel-Power\"]", eventlogSystemMicrosoftWindowsKernelPower);
+            stringpair.Add("net.if.list", netIfList);
+            stringpair.Add("perf_counter[\\234(_Total)\\1402]", perfCounter234Total1402);
+            stringpair.Add("perf_counter[\\234(_Total)\\1404]", perfCounter234Total1404);
+            stringpair.Add("perf_counter[\\2\\16]", perfCounter2_16);
+            stringpair.Add("perf_counter[\\2\\18]", perfCounter2_18);
+            stringpair.Add("perf_counter[\\2\\250]", perfCounter2_250);
+            stringpair.Add("proc.num[]", procNum);
+            stringpair.Add("system.cpu.load[percpu,avg1]", systemCpuLoadPerCpuAvg1);
+            stringpair.Add("system.cpu.load[percpu,avg5]", systemCpuLoadPerCpuAvg5);
+            stringpair.Add("system.localtime[utc]", systemLocaltimeUtc);
+            stringpair.Add("system.run[ipconfig | findstr IPv4 | sort]", systemRunIpconfigFindstrIPv4sort);
+            stringpair.Add("system.run[systeminfo,]", systemRunSysteminfo);
+            stringpair.Add("system.swap.size[,free]", systemSwapSizeFree);
+            stringpair.Add("system.swap.size[,total]", systemSwapSizeTotal);
+            stringpair.Add("system.uname", systemUname);
+            stringpair.Add("system.uptime", systemUptime);
+            stringpair.Add("vm.memory.size[free]", vmMemorySizeFree);
+            stringpair.Add("vm.memory.size[total]", vmMemorySizeTotal);
+
+            #endregion változók
+
+            List<JsonClass> jsonData = JsonConvert.DeserializeObject<List<JsonClass>>(ActiveCheckFilter(responseData));
+            string secondResponseData = "";
+            if (jsonData.Count > 0)
+            {
+                string secondMessage = "{\"request\":\"agent data\",\"session\":\"2dcf1bf2f6fc1c742812fbbf491e24f2\",\"data\":[";
+                foreach (var item in jsonData)
+                {
+                    if (stringpair.ContainsKey(item.key))
+                    {
+                        secondMessage += stringpair[item.key] + ",";
+                    }
+
+                }
+                secondMessage = secondMessage.Remove(secondMessage.Length - 1, 1);
+                secondMessage += "],\"clock\":" + clock + ",\"ns\":" + ns + "}";
+
+
+                secondResponseData = MakePacketAndConnect(secondMessage);
+
+            }
+            return secondResponseData;
+        }
+        public static string MakePacketAndConnect(string message) {
+            byte[] packet = Packet(message);
+
+            string responseData = "";
+            try
+            {
+                responseData = Connect(ZABBIX_NAME, packet, message);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("HIBA: {0}", e);
+            }
+            return responseData;
+        }
+
         public static byte[] Packet(string data)
         {
-            
+
             byte[] header = new byte[] {
             (byte)'Z', (byte)'B', (byte)'X', (byte)'D', (byte)0x00+1,
             (byte)(data.Length & 0xFF),
@@ -133,7 +167,7 @@ namespace Zabbix_TCP_Application
 
             return packet;
         }
-    
+
         public static string Connect(String server, byte[] data, string message)
         {
             try
@@ -166,7 +200,7 @@ namespace Zabbix_TCP_Application
                 // Read the first batch of the TcpServer response bytes.
                 Int32 bytes = stream.Read(data, 0, data.Length);
                 responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                //Console.WriteLine("Received: {0}\n", responseData);
+                Console.WriteLine("Received: {0}\n", responseData);
 
                 // Close everything.
                 stream.Close();
@@ -192,6 +226,7 @@ namespace Zabbix_TCP_Application
             public int delay { get; set; }
             public int lastlogsize { get; set; }
             public int mtime { get; set; }
+
         }
 
         public static string ActiveCheckFilter(string data)
@@ -218,23 +253,23 @@ namespace Zabbix_TCP_Application
             }
         }
 
-        static ulong GetTotalMemoryInBytes()
+        public static ulong GetTotalMemoryInBytes()
         {
             return new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory;
         }
 
-        static ulong GetAvailableMemoryInBytes()
+        public static ulong GetAvailableMemoryInBytes()
         {
             return new Microsoft.VisualBasic.Devices.ComputerInfo().AvailablePhysicalMemory;
         }
 
-        static string GetSystemUname()
+        public static string GetSystemUname()
         {
             //return new Microsoft.VisualBasic.Devices.ComputerInfo().OSFullName;
-            return new Microsoft.VisualBasic.Devices.Computer().Name + " "+ new Microsoft.VisualBasic.Devices.ComputerInfo().OSFullName;
+            return new Microsoft.VisualBasic.Devices.Computer().Name + " " + new Microsoft.VisualBasic.Devices.ComputerInfo().OSFullName;
         }
 
-        static int GetProcessNumber()
+        public static int GetProcessNumber()
         {
             int i = 0;
             foreach (var item in System.Diagnostics.Process.GetProcesses())
@@ -244,6 +279,31 @@ namespace Zabbix_TCP_Application
             }
             return i;
         }
+
+        public static int GetPerformanceCounter2_250() {
+            var count = Process.GetProcesses().Sum(p => p.Threads.Count);
+            return count;
+        }
+
+        
+        public static string GetUpTime()
+        {
+            var atalakit = GetTickCount64()/1000;
+            
+            return Convert.ToString(atalakit);
+        }
+
+        [DllImport("kernel32")]
+        extern static UInt64 GetTickCount64();
+
+        /*public static int GetSystemCpuLoadPerCpuAvg1()
+        {
+            var cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total", Environment.MachineName);
+            cpuCounter.NextValue();
+            System.Threading.Thread.Sleep(100); //This avoid that answer always 0
+            return (int)cpuCounter.NextValue();
+       
+        }*/
 
 
     }
